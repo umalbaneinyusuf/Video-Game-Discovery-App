67cd89653305aac3471ee9f55cbf9db575d436a5
banein-yusuf <banein.y25@gmail.com>
2025-08-09 07:11:21 +0300

    Final touches

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -11,37 +11,34 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
-  const Skeletons = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+  const skeletonCount = 20;
+  const Skeletons = Array.from({ length: skeletonCount }, (_, i) => i);
+
+  if (error) return <Text>{error}</Text>;
 
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{
-          sm: 1,
-          md: 2,
-          lg: 3,
-          xl: 4,
-        }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          Skeletons.map((Skeleton) => (
-            <GameCardContainer key={Skeleton}>
-              {" "}
-              <GameCardSkeletone />{" "}
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{
+        sm: 1,
+        md: 2,
+        lg: 3,
+        xl: 4,
+      }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        Skeletons.map((_, index) => (
+          <GameCardContainer key={index}>
+            <GameCardSkeletone />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -5,11 +5,11 @@ import {
   Image,
   List,
   ListItem,
-  Spinner,
 } from "@chakra-ui/react";
 import type { Genre } from "../hooks/useGenre";
 import useGenres from "../hooks/useGenre";
 import getCroppedImageUrl from "../Servises/image-url";
+import GenreSkeletone from "./GenreSkeletone";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -21,34 +21,40 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   if (error) return null;
 
-  if (isLoading) return <Spinner />;
-
   return (
     <>
-      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
-        {data.map((genre) => (
-          <ListItem key={genre.id} paddingY="5px">
-            <HStack>
-              <Image
-                boxSize="32px"
-                borderRadius={8}
-                objectFit="cover"
-                src={getCroppedImageUrl(genre.image_background)}
-              />
-              <Button
-                whiteSpace="normal"
-                textAlign="left"
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-                onClick={() => onSelectGenre(genre)}
-                fontSize="lg"
-                variant="link"
-              >
-                {genre.name}
-              </Button>
-            </HStack>
-          </ListItem>
-        ))}
+        {isLoading
+          ? Array.from({ length: 23 }).map((_, index) => (
+              <GenreSkeletone key={index} />
+            ))
+          : data.map((genre) => (
+              <ListItem key={genre.id} paddingY="5px">
+                <HStack>
+                  <Image
+                    boxSize="32px"
+                    borderRadius={8}
+                    objectFit="cover"
+                    src={getCroppedImageUrl(genre.image_background)}
+                  />
+                  <Button
+                    whiteSpace="normal"
+                    textAlign="left"
+                    fontWeight={
+                      genre.id === selectedGenre?.id ? "bold" : "normal"
+                    }
+                    onClick={() => onSelectGenre(genre)}
+                    fontSize="lg"
+                    variant="link"
+                  >
+                    {genre.name}
+                  </Button>
+                </HStack>
+              </ListItem>
+            ))}
       </List>
     </>
   );
