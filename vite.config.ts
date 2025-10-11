a6703dfa34c1d015e3f0a055b96414bde02d12f3
Um Al-Banein Yusuf <banein.y25@gmail.com>
2025-10-11 17:12:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Video-Game-Discovery-App/',
+  base: '/',
   plugins: [react()],
 })
